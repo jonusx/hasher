33a8619a1f1c2b589492b068d0272284da26c7bd
Mathew Cruz <jonusx@gmail.com>
2013-08-27 14:13:48 -0400

    Added unit test

---
+++ hasher Tests/hasher_Tests.m
@@ -0,0 +1,34 @@
+//
+//  hasher_Tests.m
+//  hasher Tests
+//
+//  Created by Mathew Cruz on 8/27/13.
+//  Copyright (c) 2013 Cloud.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hasher_Tests : XCTestCase
+
+@end
+
+@implementation hasher_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
